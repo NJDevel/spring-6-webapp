fe9bedc078dee339a116e74fdbd541ef7944d5d7
Abraham Leal <abraham.carlos.leal@icloud.com>
2023-02-28 13:26:34 -0500

    Adding publisher enitity (assignment)

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.respositories.AuthorRespository;
 import guru.springframework.spring6webapp.respositories.BookRepository;
+import guru.springframework.spring6webapp.respositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRespository authorRespository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRespository authorRespository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRespository authorRespository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRespository = authorRespository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -49,9 +53,19 @@ public class BootstrapData implements CommandLineRunner {
         authorRespository.save(ericSaved);
         authorRespository.save(rodSaved);
 
+        Publisher penguin = new Publisher();
+        penguin.setCity("Pittsburgh");
+        penguin.setState("PA");
+        penguin.setZip("12345");
+        penguin.setAddress("1 Iceberg Way");
+        penguin.setPublisherName("Penguin Publishing");
+
+        Publisher penguinSaved = publisherRepository.save(penguin);
+
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRespository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
     }
 }
---
+++ src/main/java/guru/springframework/spring6webapp/respositories/PublisherRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring6webapp.respositories;
+
+import guru.springframework.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * sfg course work.
+ */
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
